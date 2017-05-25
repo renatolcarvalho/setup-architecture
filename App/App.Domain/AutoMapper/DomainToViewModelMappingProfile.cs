a32c040e34d3bea1bf7982914c4c25085d37264b
Renato Luiz Carvalho <renato.carvalho@s2it.com.br>
2017-05-25 15:55:19 -0300

    Add VS Templates and Automapper

---
+++ App/App.Domain/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using App.Business.Model;
+using App.Business.ViewModel;
+using AutoMapper;
+
+namespace App.Business.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Example, ExampleViewModel>();
+        }
+    }
+}
---
+++ App/App.Domain/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using App.Business.Model;
+using App.Business.ViewModel;
+using AutoMapper;
+
+namespace App.Business.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<ExampleViewModel, Example>();
+        }
+    }
+}
